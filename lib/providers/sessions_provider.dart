af95662ab3b2439c772e049c51a5bfa63f960601
savisvajith781-create <savisvajith781@gmail.com>
2026-07-11 06:44:00 +0530

    Update sessions_provider.dart

--- lib/providers/sessions_provider.dart
+++ lib/providers/sessions_provider.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../models/session_model.dart';
 import 'core_providers.dart';
+import 'timer_provider.dart';
+import 'stats_provider.dart';
 
 final selectedHistoryDateProvider = StateProvider<DateTime>((ref) {
   return DateTime.now();
@@ -15,7 +17,7 @@ final allSessionsProvider = FutureProvider<List<SessionModel>>((ref) async {
   return ref.read(sessionRepositoryProvider).getAllSessions();
 });
 
-final sessionsByDateRangeProvider = FutureProvider.family<
+final sessionsByDateRangeProvider = FutureProvider.family
     List<SessionModel>,
     ({DateTime start, DateTime end})>((ref, range) async {
   return ref.read(sessionRepositoryProvider).getSessionsByDateRange(
@@ -29,12 +31,25 @@ class SessionsEditNotifier extends StateNotifier<AsyncValue<void>> {
 
   SessionsEditNotifier(this._ref) : super(const AsyncValue.data(null));
 
+  void _invalidateEverything() {
+    _ref.invalidate(allSessionsProvider);
+    _ref.invalidate(sessionsByDateProvider);
+    _ref.invalidate(sessionsByDateRangeProvider);
+    _ref.invalidate(todaySessionsProvider);
+    _ref.invalidate(todayTotalSecondsProvider);
+    _ref.invalidate(todaySubjectBreakdownProvider);
+    _ref.invalidate(weeklyReportProvider);
+    _ref.invalidate(heatmapDataProvider);
+    _ref.invalidate(currentStreakProvider);
+    _ref.invalidate(subjectStatsProvider);
+    _ref.invalidate(dailyStatsRangeProvider);
+  }
+
   Future<void> updateSession(SessionModel session) async {
     state = const AsyncLoading();
     try {
       await _ref.read(sessionRepositoryProvider).updateSession(session);
-      _ref.invalidate(allSessionsProvider);
-      _ref.invalidate(sessionsByDateProvider);
+      _invalidateEverything();
       state = const AsyncValue.data(null);
     } catch (e, st) {
       state = AsyncValue.error(e, st);
@@ -45,8 +60,7 @@ class SessionsEditNotifier extends StateNotifier<AsyncValue<void>> {
     state = const AsyncLoading();
     try {
       await _ref.read(sessionRepositoryProvider).deleteSession(id);
-      _ref.invalidate(allSessionsProvider);
-      _ref.invalidate(sessionsByDateProvider);
+      _invalidateEverything();
       state = const AsyncValue.data(null);
     } catch (e, st) {
       state = AsyncValue.error(e, st);
